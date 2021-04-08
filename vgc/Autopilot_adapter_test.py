ecca602fb1d142829d47465b695d5b9b30f64332
Peter Wickenberg <78222851+PeterWickenberg@users.noreply.github.com>
2021-04-08 16:20:22 +0200

    Delete Autopilot_adapter_test.py

--- vgc/Autopilot_adapter_test.py
+++
@@ -1,7 +0,0 @@
-from Autopilot_as_class import *
-
-a = vehicle(mavutil.mavlink_connection("/dev/ttyAMA0",57600))
-b = vehicle(mavutil.mavlink_connection("/dev/ttyAMA0",57600))
-print(a.get_pitch())
-print(b.get_pitch())
-print(a.get_lat())
