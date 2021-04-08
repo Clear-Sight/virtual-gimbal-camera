88845ba35d55ed97b2aa2b75f9f7f5a7da14b7af
zolo888 <34959932+zolo888@users.noreply.github.com>
2021-04-08 16:22:41 +0200

    Pylint is tested on this file

--- vgc/autopilot_test.py
+++ vgc/autopilot_test.py
@@ -1,4 +1,13 @@
-from Autopilot import *
+"""
+Test for autopilot.py
+"""
+from autopilot import get_roll
+from autopilot import get_pitch
+from autopilot import get_yaw
+from autopilot import get_lat
+from autopilot import get_lon
+from autopilot import get_alt
+from autopilot import connecting_with_autopilot
 
 def is_yaw_pitch_roll_float(vehicle):
     """Float control for the roll,pitch and yaw"""
@@ -7,7 +16,7 @@ def is_yaw_pitch_roll_float(vehicle):
     yaw   = get_yaw(vehicle)
     return isinstance(roll, float) and isinstance(pitch, float) and isinstance(yaw, float)
 
-def is_GPS_float(vehicle):
+def is_gps_float(vehicle):
     """Float control for GPS data"""
     lat  = get_lat(vehicle)
     lon = get_lon(vehicle)
@@ -18,7 +27,7 @@ def is_GPS_float(vehicle):
 def autopilot_adapter_test():
     """test all test functions"""
     vehicle = connecting_with_autopilot()
-    assert is_yaw_pitch_roll_float and is_GPS_float(vehicle)
+    assert is_yaw_pitch_roll_float and is_gps_float(vehicle)
 
 
 
