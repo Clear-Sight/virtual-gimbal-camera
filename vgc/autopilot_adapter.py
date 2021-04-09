4e1f2d6b7a0476420db3541a8afeda727c261528
Ahmad Hanash <zolo888@hotmail.com>
2021-04-09 13:44:15 +0200

    debuging and deleting autopilot from vgc

--- vgc/autopilot_adapter.py
+++ vgc/autopilot_adapter.py
@@ -1,54 +1,17 @@
 """
-Software interface for the autopilot
+Test for autopilot_adapter.py
 """
 
+def is_yaw_pitch_roll_float(self):
+    """Float control for the roll,pitch and yaw"""
+    roll  = self.get_roll()
+    pitch = self.get_pitch()
+    yaw   = self.get_yaw()
+    return isinstance(roll, float) and isinstance(pitch, float) and isinstance(yaw, float)
 
-class Vehicle:
-    """ Class Vehicle represents the autopilot as vehicle. """  
-
-    def __init__(self,connection ):
-        """ Initiates connection """
-        self.connection = connection
-
-    def get_attitude_massage(self):
-        """ Refreshes vehicle values """
-        lis = None
-        while lis is None:
-            time.sleep(0.1)
-            lis = self.connection.recv_match(type ="ATTITUDE")
-        return lis
-    
-     def get_GPS_data_massage(self):
-        """ Refreshes GPS data values """
-        lis = None
-        while lis is None:
-            time.sleep(0.1)
-            lis = self.connection.recv_match(type ="GPS_RAW_INT")
-        return lis
-
-    def get_pitch(self):
-        """ Get vehicle pitch """
-        pitch = self.get_attitude_massage().pitch
-        return float(pitch)
-
-    def get_yaw(self):
-        """ Get vehicle yaw """
-        yaw = self.get_attitude_massage().yaw
-        return float(yaw)
-
-    def get_roll(self):
-        """ Get vehicle roll """
-        roll = self.get_attitude_massage().roll
-        return float(roll)
-
-    def get_lat(self):
-        """ Get vehicle latitude """
-        return self.get_GPS_data_massage().lat
-
-    def get_lon(self):
-        """ Get vehicle longitude """
-        return self.get_GPS_data_massage().lon
-
-    def get_alt(self):
-        """ Get vehicle altitude """
-        return self.get_GPS_data_massage().alt
+def is_gps_float(self):
+    """Float control for GPS data"""
+    lat   = self.get_lat()
+    lon   = self.get_lon()
+    alt   = self.get_alt()
+    return isinstance(lat, float) and isinstance(lon, float) and isinstance(alt, float)
