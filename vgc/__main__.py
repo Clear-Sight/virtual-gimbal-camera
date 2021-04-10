a17abb0d384e696cae508d6b7469a9da28604168
AxelGard <axel.gard@protonmail.com>
2021-04-10 15:05:27 +0200

    fix of code rew

--- vgc/__main__.py
+++ vgc/__main__.py
@@ -1,10 +1,12 @@
 import cv2
-#from . import primes
-from . import buffer
 from . import io
 from . import cameraFilter
 from . import config
 
+"""
+This is the main file for virtual-gimbal-camera.
+This file is the root of the project.  
+"""
 
 __version__ = "0.1.0"
 
--- vgc/io/outputAdapter.py
+++ vgc/io/outputAdapter.py
@@ -4,6 +4,12 @@ import zmq
 from .adapter import Adapter
 from ..config import CONFIG
 
+"""
+This is the output adapter.
+This main responsibility is to send a video stream from VGC to a revicer.
+
+"""
+
 class outputAdapter(Adapter):
     def __init__(self):
         self.context = zmq.Context()
@@ -11,9 +17,6 @@ class outputAdapter(Adapter):
         self.socket.connect(f'tcp://{CONFIG["domain"]}:7777')
 
     def send(self, frame):
-        # ret, frame = self.camera.read()
-        # BUG: not needed, done in the filter
-        # frame = cv2.resize(frame, (640, 480))
         encoded, buf = cv2.imencode('.jpg', frame)
         image = base64.b64encode(buf)
         self.socket.send(image)
