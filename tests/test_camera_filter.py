3918db321e936b7dd077def8dced1d8de8acf079
Nibor9198 <robin.boregrim@gmail.com>
2021-04-16 13:39:26 +0200

    camera_filter pylint complete

--- tests/test_camera_filter.py
+++ tests/test_camera_filter.py
@@ -1,14 +1,15 @@
-"""Tests the code for camera filter. 
+"""Tests the code for camera filter.
 
 Functions:
 test_do_nothing() - Starts and stops the camera filter
-test_update_valid_input() - Tries different valid inputs to the update function.
-test_update_not_valid_input() - Tries different unvalid inputs to the update function.
-test_config() - Tests variables in the config file. 
+test_update_valid_input()
+    - Tries different valid inputs to the update function.
+test_update_not_valid_input()
+    - Tries different unvalid inputs to the update function.
+test_config() - Tests variables in the config file.
 test_handle_frame() - Tests the function handle_frame()
 """
 
-from time import sleep
 import pytest
 import numpy as np
 import cv2
@@ -20,23 +21,12 @@ from vgc import config
 
 cf = Pipeline().camera_filter
 
-#@pytest.fixture
-#def cameraFilter():
-    #return Pipeline().camera_filter
-
-#def test_not_valid_camera():
-    #cf2 = Pipeline().camera_filter
-    #with pytest.raises(ValueError, match=r".*No camera.*"):
-        #cf2.start(3)
-        #cf2.main()
-
 def test_do_nothing():
-    #"""Starts and stops the camera filter."""
+    """Starts and stops the camera filter."""
     cf.start("test.mkv")
     cf.main()
     cf.stop()
-    assert cf.stopped == True
-
+    assert cf.stopped is True
 
 def test_update_valid_input():
     """Tries different valid inputs to the update function.
@@ -44,65 +34,61 @@ def test_update_valid_input():
     First the function tries valid inputs to the update function.
     After that the function tries edge cases to the update function.
     """
-
     # Cases
-    cf.update(4, 0.5, 6) 
+    cf.update(4, 0.5, 6)
     assert cf.camera_yaw == 4
     assert cf.camera_pitch == 0.5
     assert cf.camera_zoom == 6
 
-    cf.update(128, 0.9, 6) 
+    cf.update(128, 0.9, 6)
     assert cf.camera_yaw == 128
     assert cf.camera_pitch == 0.9
     assert cf.camera_zoom == 6
 
-    cf.update(340, 0.3, 3) 
+    cf.update(340, 0.3, 3)
     assert cf.camera_yaw == 340
     assert cf.camera_pitch == 0.3
     assert cf.camera_zoom == 3
 
     #Edge cases
-
-    cf.update(360, 0.3, 3) 
+    cf.update(360, 0.3, 3)
     assert cf.camera_yaw == 360
     assert cf.camera_pitch == 0.3
     assert cf.camera_zoom == 3
 
-    cf.update(0, 0.3, 3) 
+    cf.update(0, 0.3, 3)
     assert cf.camera_yaw == 0
     assert cf.camera_pitch == 0.3
     assert cf.camera_zoom == 3
-    
-    cf.update(128, 1, 3) 
+
+    cf.update(128, 1, 3)
     assert cf.camera_yaw == 128
     assert cf.camera_pitch == 1
     assert cf.camera_zoom == 3
 
-    cf.update(128, 0, 3) 
+    cf.update(128, 0, 3)
     assert cf.camera_yaw == 128
     assert cf.camera_pitch == 0
     assert cf.camera_zoom == 3
 
-    cf.update(128, 0.3, 2) 
+    cf.update(128, 0.3, 2)
     assert cf.camera_yaw == 128
     assert cf.camera_pitch == 0.3
     assert cf.camera_zoom == 2
 
-
 def test_update_not_valid_input():
     """Tries different unvalid inputs to the update function."""
     with pytest.raises(ValueError, match=r".*camera_yaw.*"):
-        cf.update(420, 0.3, 2) 
+        cf.update(420, 0.3, 2)
     with pytest.raises(ValueError, match=r".*camera_yaw.*"):
-        cf.update(-4, 0.3, 2) 
+        cf.update(-4, 0.3, 2)
     with pytest.raises(ValueError, match=r".*camera_pitch.*"):
-        cf.update(150, 1.5, 2) 
+        cf.update(150, 1.5, 2)
     with pytest.raises(ValueError, match=r".*camera_pitch.*"):
-        cf.update(266, -0.5, 2) 
+        cf.update(266, -0.5, 2)
     with pytest.raises(ValueError, match=r".*camera_zoom.*"):
-        cf.update(167, 0.3, 1) 
+        cf.update(167, 0.3, 1)
 
-    
 def test_config():
     """Tests variables in the config file. """
     cam_width = config.CONFIG['cam_width']
@@ -111,20 +97,20 @@ def test_config():
     cam_height = config.CONFIG['cam_height']
     assert cam_height >= 0
 
-    cam_input = config.CONFIG['cam_input']
-
 def test_handle_frame():
     """Tests the function handle_frame().
-    
-    The function creates a matrix and puts it in 
-    the handle_frame function. Then it tests for the
-    right output. 
+
+    The function creates an image "img" and applies
+    the handle_frame function to it with different parameteres.
+    Then it tests if the function gave the expected output.
     """
     size = 50
     img = np.zeros((size,size,3), dtype=np.uint8)
     for pixelx in range(size):
         for pixely in range(size):
-            img[pixelx][pixely] = [int((pixelx/size) * 255), int((pixely /size) * 255),  int(((pixelx + pixely) /(2*size)) * 255)]
+            img[pixelx][pixely] = [int((pixelx/size) * 255),
+                                   int((pixely /size) * 255),
+                                   int(((pixelx + pixely) /(2*size)) * 255)]
 
     for pixel in range(size):
         img[pixel][size//4] =  255
@@ -133,16 +119,20 @@ def test_handle_frame():
         img[size//2][pixel] =  255
         img[pixel][size*3//4] =  255
         img[size*3//4][pixel] =  255
-    
+
     tests = [[35, 1, 2], [120, 0, 4], [128, 0.9, 6], [340, 0.3, 3],
             [360, 0.5, 2], [4, 0.25, 3], [200, 0.2, 5], [0, 0.8, 4]
             ]
     for test in tests:
         cf.update(test[0],test[1],test[2])
-        frame = cf.handle_frame(img, img.shape[0], img.shape[1],img.shape[0], img.shape[1])
+        frame = cf.handle_frame(img, img.shape[0], img.shape[1],
+                                     img.shape[0], img.shape[1])
 
-        #frame.dump(f'tests/test_images/matrix{test[0]}-{test[1]}-{test[2]}.dat')
-        loaded = np.load(f'tests/test_images/matrix{test[0]}-{test[1]}-{test[2]}.dat', allow_pickle=True)
+        #frame.dump(
+        #   f'tests/test_images/matrix{test[0]}-{test[1]}-{test[2]}.dat')
+        loaded = np.load(
+            f'tests/test_images/matrix{test[0]}-{test[1]}-{test[2]}.dat',
+            allow_pickle=True)
 
         #cv2.imshow('source',img)
         #cv2.imshow('image',frame)
--- vgc/camera_filter.py
+++ vgc/camera_filter.py
@@ -8,7 +8,8 @@ Functions in CameraFiler:
     update(self, camera_yaw , camera_pitch, camera_zoom) - Updates variables
     stop(self) - Stops the camera filter
     start(self) - Starts the camera filter
-    handle_frame(self, frame, frame_width, frame_height, out_width, out_height) - Crops and resizes a frame. 
+    handle_frame(self, frame, frame_width, frame_height,
+                 out_width, out_height) - Crops and resizes a frame.
 """
 import threading
 import cv2
@@ -31,11 +32,14 @@ class CameraFilter:
 
         Functions in the class:
             __init__(self) - Initialize variables
-            update(self, camera_yaw , camera_, camera_zoom) - Updates variables
+            update(self, camera_yaw , camera_, camera_zoom)
+                                        - Updates variables
             stop(self) - Stops the camera filter
             start(self) - Starts the camera filter
             main(self) - Main loop of the camerafilter
-            handle_frame(self, frame, frame_width, frame_height, out_width, out_height) - Crops and resizes a frame. 
+            handle_frame(self, frame, frame_width, frame_height,
+                                            out_width, out_height)
+                                        - Crops and resizes a frame.
     """
 
     def __init__(self, pipeline):
@@ -54,6 +58,7 @@ class CameraFilter:
         # Set Defaults
         self.camera_yaw , self.camera_pitch, self.camera_zoom = 0,0,4
         self.stopped = False
+        self.camera_input = 0
 
         # Get ouptuptAdapter
         self.output_adapter = output_adapter.OutputAdapter()
@@ -61,19 +66,19 @@ class CameraFilter:
     def update(self, camera_yaw , camera_pitch, camera_zoom):
         """Updates the cropping values of the CameraFilter."""
 
-        if not (0 <= camera_yaw <= 360):
+        if not 0 <= camera_yaw <= 360:
             raise ValueError("camera_yaw out of bounds (0 to 360)")
 
-        if not (0 <= camera_pitch <= 1):
+        if not 0 <= camera_pitch <= 1:
             raise ValueError("camera_pitch out of bounds (0 to 1)")
 
-        if not (2 <= camera_zoom):
+        if not 2 <= camera_zoom:
             raise ValueError("camera_zoom out of bounds ( >= 2)")
 
         self.semaphore.acquire()
-        self.camera_yaw  = camera_yaw  
-        self.camera_pitch = camera_pitch 
-        self.camera_zoom = camera_zoom 
+        self.camera_yaw  = camera_yaw
+        self.camera_pitch = camera_pitch
+        self.camera_zoom = camera_zoom
         self.semaphore.release()
 
     def stop(self):
@@ -95,10 +100,12 @@ class CameraFilter:
         """The main function of the class.
 
         Takes input videostream input.
-        Crops and rotates according to camera_yaw , camera_pitch and camera_zoom.
+        Crops and rotates according to camera_yaw ,
+        camera_pitch and camera_zoom.
         Outputs the proccesed videostream.
         """
-        cap = cv2.VideoCapture(self.camera_input if self.camera_input else config.CONFIG['cam_input'])
+        cap = cv2.VideoCapture(self.camera_input if self.camera_input
+                                            else config.CONFIG['cam_input'])
 
         if not cap.isOpened():
             raise ValueError("No camera")
@@ -119,10 +126,12 @@ class CameraFilter:
             cnt += 1  # Counting the frames
 
             # Avoid problems when video finish
-            if not ret: break
+            if not ret:
+                break
 
             self.semaphore.acquire() # Get rotation matrix
-            final_frame = self.handle_frame(frame, frame_width, frame_height, width, height)
+            final_frame = self.handle_frame(frame, frame_width,
+                                            frame_height, width, height)
             try:
                 self.output_adapter.send(final_frame)
                 cv2.waitKey(1)
@@ -134,12 +143,14 @@ class CameraFilter:
 
         cv2.destroyAllWindows()
 
-    def handle_frame(self, frame, frame_width, frame_height, out_width, out_height):
-        """Handles the frame. 
+    def handle_frame(self, frame, frame_width, frame_height,
+                                    out_width, out_height):
+        """Handles the frame.
 
-        Takes a frame, the width and height of the frame, and the width and the height 
-        of the croped frame as an input. Then the function converts the frame to a matrix, 
-        rotates it, and the crops the frame. The function returns a croped resized frame.  
+        Takes a frame, the width and height of the frame,
+        and the width and the height of the croped frame as an input.
+        Then the function converts the frame to a matrix, rotates it,
+        and the crops the frame. The function returns a croped resized frame.
         """
         matrix = cv2.getRotationMatrix2D((frame_width/2, frame_height/2),
                                         self.camera_yaw , 1)
@@ -147,7 +158,8 @@ class CameraFilter:
         rotated_frame = cv2.warpAffine(frame, matrix,
                                         (frame_width, frame_height))
         # Crop the frame
-        relative_pitch = (frame_height/2 - (out_height/2)/self.camera_zoom) * self.camera_pitch 
+        relative_pitch = (frame_height/2 -
+                          (out_height/2)/self.camera_zoom)* self.camera_pitch
 
         crop_frame = cv2.getRectSubPix(rotated_frame,
                                         (int(out_width/self.camera_zoom),
