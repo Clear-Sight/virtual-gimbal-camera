76f3b3d5ed110db94f159a2ffa3cd148228263de
AxelGard <axel.gard@protonmail.com>
2021-04-18 17:00:33 +0200

    added future lxml to install

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sudo ln -s ${PWD}/run.sh ~/.config/autostart
 python -m pip install --upgrade pip
+pip3 install --upgrade future lxml
 pip3 install pytest
 pip3 install -r requirements.txt
 python3 -m pip install --upgrade pymavlink
