4704771a878896fe7f54512f10b4814e62caaf4f
AxelGard <axel.gard@protonmail.com>
2021-04-20 13:42:15 +0200

    added C compi step in install

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sudo ln -s ${PWD}/run.sh ~/.config/autostart
+gcc -shared -o ./vgc/trigonometric_fast.so -fPIC ./vgc/trigonometric_fast.c
 python -m pip install --upgrade pip
 pip3 install --upgrade future lxml
 pip3 install pytest
