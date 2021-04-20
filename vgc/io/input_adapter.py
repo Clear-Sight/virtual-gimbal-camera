300a08ba2ddc8431af8b86913361af1228d138b1
AxelGard <axel.gard@protonmail.com>
2021-04-20 14:34:02 +0200

    added try and catch on requests in case of death

--- vgc/io/input_adapter.py
+++ vgc/io/input_adapter.py
@@ -25,9 +25,12 @@ class InputAdapter:
 
     def get_usr_input(self):
         """ Fetch user input from web server via GET request. """
-        r = requests.get(
-        f'http://{CONFIG["input_domain"]}/drone/user/fetch')
-        return r.json()
+        try:
+            r = requests.get(
+            f'http://{CONFIG["input_domain"]}/drone/user/fetch')
+            return r.json()
+        except:
+            return self.cached_usr_msg
 
     def push(self):
         """
