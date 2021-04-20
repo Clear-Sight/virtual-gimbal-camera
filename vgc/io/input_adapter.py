fbc2e2aaeeb9cfcff59f7e9d3683e1c043dc5474
AxelGard <axel.gard@protonmail.com>
2021-04-20 14:48:42 +0200

    new fix for check for status code

--- vgc/io/input_adapter.py
+++ vgc/io/input_adapter.py
@@ -25,12 +25,14 @@ class InputAdapter:
 
     def get_usr_input(self):
         """ Fetch user input from web server via GET request. """
-        try:
-            r = requests.get(
+        msg = self.cached_usr_msg
+        r = requests.get(
             f'http://{CONFIG["input_domain"]}/drone/user/fetch')
-            return r.json()
-        except:
+            msg = r.json()
+        if r.status_code != 200:
             return self.cached_usr_msg
+        else:
+            return msg
 
     def push(self):
         """
