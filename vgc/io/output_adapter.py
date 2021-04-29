1a70cd66840444e3734918a4a6af894c6d91b3ec
AxelGard <axel.gard@protonmail.com>
2021-04-29 19:26:46 +0200

    fix in output

--- vgc/io/output_adapter.py
+++ vgc/io/output_adapter.py
@@ -1,13 +1,16 @@
+"""
+This is the output adapter.
+This main responsibility is to send a
+video stream from VGC to a revicer.
+
+"""
+
 import base64
 import cv2
 import zmq
 from ..config import CONFIG
 
-"""
-This is the output adapter.
-This main responsibility is to send a video stream from VGC to a revicer.
 
-"""
 
 class OutputAdapter:
     """ Is given frames and sends them to the set domain """
@@ -21,3 +24,6 @@ class OutputAdapter:
         encoded, buf = cv2.imencode('.jpg', frame)
         image = base64.b64encode(buf)
         self.socket.send(image)
+
+    def __del__(self):
+        self.socket.__del__()
