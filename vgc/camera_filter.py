2869dd85fe4570ee7cc87dc306e412f6f418535e
wikstorm <carl_wikstrom@hotmail.com>
2021-04-29 19:17:09 +0200

    Fixed pylint in camera_filter

--- vgc/camera_filter.py
+++ vgc/camera_filter.py
@@ -11,10 +11,9 @@ Functions in CameraFiler:
     handle_frame(self, frame, frame_width, frame_height,
                  out_width, out_height) - Crops and resizes a frame.
 """
-from .log import logger
 import threading
 import cv2
-
+from .log import logger
 from .io import output_adapter
 from . import config
 
@@ -57,7 +56,8 @@ class CameraFilter:
         self.thread = threading.Thread(target=self.main)
 
         # Set Defaults
-        self.camera_yaw , self.camera_pitch, self.camera_zoom, self.camera_roll = 0,0,4,0
+        self.camera_yaw , self.camera_pitch, self.camera_zoom,\
+        self.camera_roll = 0,0,4,0
         self.stopped = False
         self.camera_input = 0
 
@@ -68,19 +68,7 @@ class CameraFilter:
     def update(self, camera_yaw , camera_pitch, camera_zoom, camera_roll=0):
 
         """Updates the cropping values of the CameraFilter."""
-        """
-        if not 0 <= camera_yaw <= 360:
-            raise ValueError("camera_yaw out of bounds (0 to 360)")
 
-        if not 0 <= camera_pitch <= 1:
-            raise ValueError("camera_pitch out of bounds (0 to 1)")
-
-        if not 2 <= camera_zoom:
-            raise ValueError("camera_zoom out of bounds ( >= 2)")
-
-        if not 0 <= camera_roll <= 360:
-            raise ValueError("camera_zoom out of bounds ( >= 2)")
-        """
         self.semaphore.acquire()
 
         self.camera_yaw  = camera_yaw
@@ -99,7 +87,8 @@ class CameraFilter:
     def start(self, camera_input=0):
         """Starts the Camerafilter."""
         # Set defaults
-        self.camera_yaw , self.camera_pitch, self.camera_zoom, self.camera_roll = 0,0,4,0
+        self.camera_yaw, self.camera_pitch, self.camera_zoom,\
+        self.camera_roll = 0,0,4,0
         self.stopped = False
         self.camera_input = camera_input
 
@@ -153,8 +142,6 @@ class CameraFilter:
             self.semaphore.release()
 
         cap.release()
-
-
         cv2.destroyAllWindows()
 
     def handle_frame(self, frame, frame_width, frame_height,
@@ -171,10 +158,13 @@ class CameraFilter:
         # Apply rotation matrix
         rotated_frame = cv2.warpAffine(frame, matrix,
                                         (frame_width, frame_height))
-        relative_pitch = (frame_height/2 - (out_height/2)/self.camera_zoom)* self.camera_pitch
+        relative_pitch = (frame_height/2 -\
+        (out_height/2)/self.camera_zoom)*self.camera_pitch
 
-        matrix_roll = cv2.getRotationMatrix2D((frame_width/2, frame_height/2 - relative_pitch), self.camera_roll, 1)
-        rotated_frame = cv2.warpAffine(rotated_frame, matrix_roll, (frame_width,frame_height))
+        matrix_roll = cv2.getRotationMatrix2D((frame_width/2,
+        frame_height/2 - relative_pitch), self.camera_roll, 1)
+        rotated_frame = cv2.warpAffine(rotated_frame, matrix_roll,
+        (frame_width,frame_height))
 
         # Crop the frame
 
