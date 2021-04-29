40b935690a37c2284ef137f3ce00f44498ee96f6
AxelGard <axel.gard@protonmail.com>
2021-04-29 19:39:50 +0200

    fixt pylint conf

--- vgc/config.py
+++ vgc/config.py
@@ -1,9 +1,11 @@
+"""
+loads and keeps the config for the system
+"""
 import json
 
 def load_config():
     """ returns a dict of the config from config.json """
-    CONF_FILE = "./config.json"
-    with open(CONF_FILE, 'r') as file:
+    with open("./config.json", 'r') as file:
         conf = json.load(file)
     return conf
 
