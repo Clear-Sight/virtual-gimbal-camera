430f562e5409217ce763e382264c06f5bf0aee43
AxelGard <axel.gard@protonmail.com>
2021-04-29 19:37:01 +0200

    fixt pylint main

--- vgc/__main__.py
+++ vgc/__main__.py
@@ -1,15 +1,18 @@
-from .config import CONFIG
-from .pipeline import Pipeline
-
 """
 This is the main file for virtual-gimbal-camera.
 This file is the root of the project.
 """
 
+from .pipeline import Pipeline
+
 __version__ = "1.0.0"
 
 
 def main():
+    """
+    initalise the systems by s
+    tarting all the treads
+    """
     pipeline = Pipeline()
     pipeline.start()
 
