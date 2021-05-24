984b78eb2314195710f74f035320be5d5a74ab4a
AxelGard <axel.gard@protonmail.com>
2021-05-24 14:27:13 +0200

    log to WARNING

--- vgc/log.py
+++ vgc/log.py
@@ -11,11 +11,11 @@ import os
 
 date = datetime.now().strftime("%m-%d-%Y")
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.WARNING)
 formatter = logging.Formatter('%(levelname)s:%(name)s:%(message)s')
 log_filename = f'vgc/.logs/vgc-{date}.log'
 os.makedirs(os.path.dirname(log_filename), exist_ok=True)
 file_handler = logging.FileHandler(log_filename)
-file_handler.setLevel(logging.INFO)
+file_handler.setLevel(logging.WARNING)
 file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
